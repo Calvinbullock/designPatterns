c7b6473abbfaebcc73e9eabcb360e66339163c30
Calvinbullock <calvinnbullock@gmail.com>
2025-01-29 10:17:01 -0700

    add drawing strat.h

---
+++ 331.Skeet/DrawingStrat.h
@@ -0,0 +1,90 @@
+#pragma once
+
+//
+// NOTE: Base
+//
+class DrawingStratagy
+{
+
+public:
+   //
+   // Constructors
+   DrawingStratagy() {}
+
+   //
+   // Getters / Setters
+
+private:
+};
+
+
+//
+// NOTE: Standard
+//
+class StandardStratagy
+{
+
+public:
+   //
+   // Constructors
+   StandardStratagy() {}
+
+   //
+   // Getters / Setters
+
+private:
+};
+
+
+//
+// NOTE: Floater
+//
+class FloaterStatagy
+{
+
+public:
+   //
+   // Constructors
+   FloaterStatagy() {}
+
+   //
+   // Getters / Setters
+
+private:
+};
+
+
+//
+// NOTE: Crazy
+//
+class CrazyStratagy
+{
+
+public:
+   //
+   // Constructors
+   CrazyStratagy() {}
+
+   //
+   // Getters / Setters
+
+private:
+};
+
+
+//
+// NOTE: Sinker
+//
+class SinkerStatagy
+{
+
+public:
+   //
+   // Constructors
+   SinkerStatagy() {}
+
+   //
+   // Getters / Setters
+
+private:
+};
